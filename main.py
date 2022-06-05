55746b8414b6cc360f8aa681478714f98e769382
dasha <dasha0927276@gmail.com>
2022-06-05 18:33:19 +0300

    proize and delet

---
+++ main.py
@@ -0,0 +1,14 @@
+def proize(a,b):
+    return a*b
+def delet(a,b):
+    return a/b
+def proverka():
+    a = int(input())
+    b = int(input())
+    c = input("*,/ \n",)
+    if c == "*":
+        print(proize(a,b))
+    elif c == "/":
+        print(delet(a,b))
+
+proverka()
